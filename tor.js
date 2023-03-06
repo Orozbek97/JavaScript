27b4a93e508e7d1d81d7217906e8013d62c5deb3
Orozbek97 <tagaev.orozbek@gmail.com>
2023-03-07 04:37:42 +0600

    javascript

---
+++ tor.js
@@ -0,0 +1,34 @@
+// let a = 4
+// let b = 55
+//  console.log(a + b)
+
+// let a = 4
+// let b = 55
+// console.log( a * b )
+
+// let a = 4
+// let b = 55
+// console.log( a / b )
+
+// let a = 4
+// let b = 55
+// console.log( a % b )
+
+
+// let a = 'hello '
+// let b = 'MSI'
+//  console.log ( a + b )
+
+// let a = 'hello '
+// let b = 5
+//  console.log ( a * b )
+
+
+// let a = 5
+// let b = '5'
+//  console.log( (a-3)  * b)
+
+// let a = 5
+// let b = '5'
+// c = a - 3
+// console.log( c * b )
