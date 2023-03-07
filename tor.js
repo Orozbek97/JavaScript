74da5d9c3a9e836ac361452642306e3296ab254c
Orozbek97 <tagaev.orozbek@gmail.com>
2023-03-08 05:08:31 +0600

    homework18

--- tor.js
+++ tor.js
@@ -1,3 +1,6 @@
+
+                        // HOMEWORK 06.03.2023
+
 // let a = 4
 // let b = 55
 //  console.log(a + b)
@@ -32,3 +35,105 @@
 // let b = '5'
 // c = a - 3
 // console.log( c * b )
+
+
+/////-----/////-----/////-----/////-----/////-----/////-----/////-----/////-----/////-----/////-----/////-----////
+
+
+// let a = 'hello world'
+// // let b = 'great day'
+// // let c = b.slice(0,b.indexOf('d'))
+// // console.log (c,a.slice(a.indexOf('w')))
+//
+// let a = 'word hello'
+// console.log(a.replaceAll('l' , 'k') )
+
+/////-----/////-----/////-----/////-----/////-----/////-----/////-----/////-----/////-----/////-----/////-----/////
+//                                            HOMEWORK.... 07.03.2023
+
+// let a = 'hello'                                                    // Hello ivan
+// let b = 'Ivan'
+// c = a.slice(0 , 1)
+// d = a.slice( 1 )
+// e = c.toUpperCase()
+//  console.log (e + d + " " + b )
+
+
+// let a = 'lorem ipsum dolor'                                          // записать длину в переменную b
+// b = a.length
+// let c = ' symbols'
+// console.log(a +' ' +   b + c )
+
+
+// let a = 'lorem ipsum dolor sit amet'                                    // вернуть sit lorem ipsum dolor amet
+// b = a.slice(18 , 22 )
+// c = a.replace('sit', '')
+// console.log( b + c )
+
+
+// let a = 'children'                                                         //вернуть CHILDREn
+// b = a.slice(0 , 7)
+// c = a.slice(7)
+// console.log(b.toUpperCase()+c)
+
+
+// let a = 'boss'                                                              // вернуть bsso
+// b = a.replace('o' , 's')
+// c = b.slice(3 ,4)
+// d = b.slice(0 ,3)
+// console.log(d + c.replace( 's', 'o'))
+
+
+// let a = 'sunday'                                                               // вернуть index d
+// b = a.slice(3 ,4)
+// console.log(b)
+
+
+// let a = 'hello'                                                               // вернуть hekso
+// b = a.replace('l' , 'k')
+// c = b.replace('l' , 's')
+// console.log (c)
+
+
+// let a = 'user'                                                       // вернуть new us
+// let b = 'new'
+// c = a.slice(a.indexOf('u') , a.indexOf('e'))
+// console.log(b , c)
+
+
+// let a = 'monday'                                                     // вернуть daydaymon
+// let b = 'day'
+// c = a.slice(a.indexOf('m') , a.indexOf('d'))
+// console.log(b+b+c)
+
+
+// let a = 'HELLO'                                                      // вернуть 'hElLo'
+// b = a.slice(0 , 1)
+// c = a.slice(1 ,2)
+// d = a.slice(2 ,3)
+// e = a.slice(3 ,4)
+// f = a.slice(4 ,5)
+// g = b.toLowerCase()
+// h = d.toLowerCase()
+// i = f.toLowerCase()
+// console.log (g + c + h + e +  i)
+
+
+
+// let a = 5                                                            // вернуть '450'
+// let b = '400'
+// c = b.substring(0 , 1)
+// d = b.substring(2 , 3)
+// console.log(c + a + d)
+
+
+// let a = 'monkey'                                                      // вернуть manak
+// let b = 'banana'
+// c = a.substring(0 ,1)
+// d = a.substring(3 ,4)
+// e = b.substring(1,4)
+// console.log(c + e + d)
+
+
+
+
